74c1e6a72dae994be3fd6f696a70599047966528
Vicente Jr <137561060+vicentejrprog@users.noreply.github.com>
2025-11-21 19:33:09 +0000

    adiciona scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Conhecimento em novas ferramentas
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-function Home(){
-    return(
-     <>
-     <h2>Se amar é mudar a alma de casa<br/>
-      Quero que a minha viva pagando<br />
-      Aluguel no seu coração para sempre.</h2>
-     </>
-    );
+function Home() {
+  return (
+    <>
+      <h2>
+        Se amar é mudar a alma de casa
+        <br />
+        Quero que a minha viva pagando
+        <br />
+        Aluguel no seu coração para sempre.
+      </h2>
+    </>
+  );
+}
+
+function Teste() {
+  console.log("Teste 1");
+}
+
+function Teste2() {
+  console.log("Teste 2");
 }
 
 export default Home;

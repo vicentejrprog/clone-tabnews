4bd40efccea3a54b00f584aa091f345c5fa553b8
Vicente Jr <137561060+vicentejrprog@users.noreply.github.com>
2025-12-18 17:36:32 +0000

    teste do aiven em prd

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   console.log("Credenciais do Postgres:", {
